14d788297bb6057e69fd581445c1f4c697c13e11
yatskar <ars_yat@mail.ru>
2018-12-08 20:04:41 +0400

    parsing lat long

--- plot_geo.py
+++ plot_geo.py
@@ -1,14 +1,21 @@
 import csv
 
-file = open("data\CALM_Summary_table.csv")
+def convert(degree):
+    degree = degree.strip(',').\
+        replace('\x94',"").replace(","," ").replace("'", " ").replace("°", " ").replace('"', " ").replace("  ", " ").strip()
+
+    multiplier = 1 if degree[-1] in ['N', 'E'] else -1
+    s = degree[:-1].strip().split(' ')
+    return multiplier * sum(float(x) / 60 ** n for n, x in enumerate(s))
+
+file = open("data//CALM_Summary_table_utf8.csv")
 reader = csv.reader(file, delimiter = ';')
 
-row_num = 0
 for row in reader:
+    if not row[1] in {"United States (Alaska)", "CANADA", "Russia"}:
+        continue
     print(', '.join(row))
-    row_num += 1
-    if row_num <= 2: continue
-    lat = row[4]
-    long = row[5]
+    lat = convert(row[4])
+    long = convert(row[5])
     # for x in range(7, )
 
